b2bcad7d0f648570cf49b89baf56999e5e9abd02
Swantayzaaung <dockyawkyaw@gmail.com>
2024-04-03 15:48:12 +0700

    created about and index pages

--- marker_web/views.py
+++ marker_web/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    available_years
     return render(request, "marker_web/index.html")
+
+def about(request):
+    return render(request, "marker_web/about.html")
+
+def practice(request):
+    return render(request, "marker_web/practice.html")
+
+def results(request):
+    return render(request, "marker_web/results.html")
